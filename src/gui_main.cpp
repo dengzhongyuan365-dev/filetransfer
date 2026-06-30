716752475d265b030da71d22621146935189d2c1
dengzhongyuan <dengzhongyuan@uniontech.com>
2026-06-30 20:14:02 +0800

    feat: add deb packaging assets

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -1,9 +1,23 @@
 #include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 #include "gui/main_window.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+    QTranslator translator;
+    const QString locale = QLocale::system().name();
+#ifdef LAN_TRANSLATION_DIR
+    const bool translation_loaded = translator.load("lan-file-transfer_" + locale, LAN_TRANSLATION_DIR);
+#else
+    const bool translation_loaded = translator.load("lan-file-transfer_" + locale, "translations");
+#endif
+    if (translation_loaded) {
+        app.installTranslator(&translator);
+    }
+
     lan::gui::MainWindow window;
     window.show();
     return app.exec();
---
+++ translations/lan-file-transfer_zh_CN.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>LAN File Transfer</source>
+        <translation>局域网文件传输</translation>
+    </message>
+</context>
+</TS>
